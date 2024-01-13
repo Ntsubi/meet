4081020997df283f81ec8686b3058ecf2f875e0b
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2024-01-13 12:25:17 +0100

    Cleaned up formatting

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -9,14 +9,14 @@ describe('<EventList /> component', () => {
     EventListComponent = render(<EventList />);
   })
 
-  test('has element with "list" role', () => {
-    expect(EventListComponent.queryByRole('list')).toBeInTheDocument();
-  })
-
+  test('has an element with "list" role', () => {
+    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  });
 
   test('renders correct number of events', () => {
-    EventListComponent.rerender(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
+    EventListComponent.rerender(<EventList events={
+      [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
+    } />);
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
-
