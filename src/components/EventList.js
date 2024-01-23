0c45e21b06d42a7d2e4a657a3fe1c6fe6a0fe537
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2024-01-23 17:19:36 +0100

    Created EventList component and wrote tests

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,6 +1,6 @@
 import Event from './Event';
 
-const EventList = ({ events }) => {  //the events prop comes from the mock data populated in line 12 of EventList.test.js
+const EventList = ({ events }) => {
   return (
     <ul id="event-list">
       {events ? events.map(event => <Event event={event} key={event.id} />) : null}
