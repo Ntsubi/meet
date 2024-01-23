7d04a8c8039a58473d807481414c3ff6aac977ee
Ntsubi <145991816+Ntsubi@users.noreply.github.com>
2024-01-23 17:19:21 +0100

    Created Event component and wrote tests

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,25 @@
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({ event }) => {
+
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <li></li>
+    <li className="event">
+      <h4>{event.summary}</h4>
+      <p>{event.created}</p>
+      <p>{event.location}</p>
+      <button className="details-button"
+        onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+      {showDetails ? (
+        <div className="details">
+          <h3>Event Details</h3>
+          <p>{event.description}</p>
+        </div>
+      ) : null}
+    </li>
   );
 }
 
